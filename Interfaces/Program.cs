fbf12d0c7221006016b5076db0e4e78416cf4eeb
Francis Boakye <fboakye018@st.ug.edu.gh>
2025-08-05 15:32:13 +0000

    Completing of the Interfaces Task

---
+++ Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿//Interfaces and Classes Example                 ID: 11317514
+
+namespace Interfaces
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
