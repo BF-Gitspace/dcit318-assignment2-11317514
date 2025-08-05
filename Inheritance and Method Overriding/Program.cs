4da1f48f241c6b4655af0c10b45dc4d24b3743ce
Francis Boakye <fboakye018@st.ug.edu.gh>
2025-08-05 14:12:09 +0000

    Completion of the Inheritance and method Overriding task

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,45 @@
+﻿// Assignment 2 (i): Inheritance and Method Overriding        ID: 11317514.
+
+namespace Inheritance_and_Method_Overriding
+{
+    internal class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    internal class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here we are going to demonstrate method overriding in inheritance
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); 
+            dog.MakeSound();           
+            cat.MakeSound();
+            Console.ReadKey();
+        }
+         // Keep the console window open
+    }
+}
